697daa6f778c7b8721e7386817bd6dd3373a082c
Liam Dawson <liamrdawson@gmail.com>
2019-04-04 18:53:52 +0100

    add 10 second  interval for background image transition

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+const hero1 = document.querySelector(`.hero1`);
+const hero2 = document.querySelector(`.hero2`);
+const hero3 = document.querySelector(`.hero3`);
+const images = [
+    hero3, 
+    hero2, 
+    hero1,
+]
+
+setInterval(() => {
+    let current = 0;
+    for (var i = 0; i < images.length; i++) {
+        images[i].style.opacity = 0;
+    }
+    current = (current != images.length - 1) ? current + 1 : 0;
+    images[current].style.opacity = 1;
+}, 10000);
