78e89f3018981fc1295a527044f235450a78e37a
Liam Dawson <liamrdawson@gmail.com>
2019-06-30 19:40:06 +0100

    remove close icon

--- JS/gal.js
+++ JS/gal.js
@@ -5,10 +5,10 @@ const imagesGroup  = document.querySelectorAll(`.images img`);
 const lightbox  = document.querySelector(`#lightbox`);
 const previous = document.querySelector(`#previous`);
 const next = document.querySelector(`#next`);
-const closeGal = document.querySelector(`.close-icon`);
+const closeGal = document.querySelector(`.close`);
 const body = document.querySelector(`body`);
-const bar1 = document.querySelector(`.bar-1`);
-const bar2 = document.querySelector(`.bar-2`);
+// const bar1 = document.querySelector(`.bar-1`);
+// const bar2 = document.querySelector(`.bar-2`);
 
 const arrayOfImages = [...imagesGroup];
 
@@ -33,7 +33,7 @@ function imgClick(e) {
 lightbox.addEventListener(`click`, clickOut);
 
 function clickOut(e) {
-    if (e.target === lightbox || e.target === closeGal || e.target === bar1 || e.target === bar2) {
+    if (e.target === lightbox || e.target === closeGal) {
         lightbox.style.visibility = `hidden`;
         body.style.overflowY = `visible`;
     }
