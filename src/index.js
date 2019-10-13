930f7da4ee356a2349a38fbadbfd28ce671ac740
Liam Dawson <liamrdawson@gmail.com>
2019-10-13 13:29:01 +0100

    add first loaders to handle CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {slides} from './JS/slideshow';
 import {navSlideIn} from './JS/navSlideIn';
 import {eventListeners} from './JS/gal';
+import '../CSS/style.css';
 
 alert("HELLO FROM WEBPACK!");
 
